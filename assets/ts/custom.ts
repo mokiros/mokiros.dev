b483380903ecde69a4b89f07254b96c9a9cab6db
mokiros <Mokiros@yandex.ru>
2024-03-02 02:00:11 +0300

    Custom assets

---
+++ assets/ts/custom.ts
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+	var d = new Date(Date.now() - 967896000000)
+	var a = String(Math.abs(d.getUTCFullYear() - 1970))
+	var cag = document.getElementsByClassName('cag')
+
+	for (var i = 0; i < cag.length; i++){
+		cag[i].innerHTML = a
+	}
+})
